700e0a8d091ef6c7d427422ef9df65df865dd2a0
Lei Chen <chenleicoder@foxmail.com>
2021-10-22 11:05:30 +0800

    chore: bump wxml-parser and add unit test (#1)

--- tests/rules/report-wxml-syntax-error.js
+++ tests/rules/report-wxml-syntax-error.js
@@ -11,6 +11,42 @@ tester.run("report-wxml-syntax-error", rule, {
       filename: "wxml.wxml",
       code: `<app> <main-page /> </app>`,
     },
+    {
+      filename: "wxml.wxml",
+      code: `<app> <main-page /> </ app >`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<wxs module="utils" ></wxs>`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<wxs module="utils" >  </ wxs>`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<wxs module="utils" > </ wxs >`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<wxs module="utils" >  </wxs >`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<block wx:if="{{currentIndex === index && item.status <= 2}}" ></block>`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<view>{{ index < 3 : "{{}}</>" : '></}}{{' }}</view>"`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `<view>{{ }} {{ "" }} {{show}} {{ a < 90}}</view>"`,
+    },
+    {
+      filename: "wxml.wxml",
+      code: `{{ }} {{ "" }} {{show}} {{ a < 90}} }}"`,
+    },
   ],
   invalid: [
     {
@@ -28,12 +64,66 @@ tester.run("report-wxml-syntax-error", rule, {
     },
     {
       filename: "wxml.wxml",
+      code: `<wxs module="utils" /> {{ a > ' }} {{}}</wxs>`,
+      errors: [
+        {
+          messageId: "wxmlError",
+          data: {
+            error:
+              "Expecting token of type --> EOF <-- but found --> '{{ a > ' }} {{}}' <--",
+          },
+        },
+      ],
+    },
+    {
+      filename: "wxml.wxml",
+      code: `
+        <app key="value">
+          {{ nihao '" }}
+        </app>
+      `,
+      errors: [
+        {
+          messageId: "wxmlError",
+          data: {
+            error:
+              "unexpected character: ->'<- at offset: 46, skipped 2 characters.",
+          },
+        },
+      ],
+    },
+    {
+      filename: "wxml.wxml",
       code: `<missing-close / <share-modal /> <bottom-bar />`,
       errors: [
         {
           messageId: "wxmlError",
           data: {
             error: `Expecting: one of these possible Token sequences:\n  1. [CLOSE]\n  2. [SLASH_CLOSE]\nbut found: '/'`,
+          },
+        },
+      ],
+    },
+    {
+      filename: "wxml.wxml",
+      code: `{{}`,
+      errors: [
+        {
+          messageId: "wxmlError",
+          data: {
+            error: `wx interpolation unexpected end`,
+          },
+        },
+      ],
+    },
+    {
+      filename: "wxml.wxml",
+      code: `{{`,
+      errors: [
+        {
+          messageId: "wxmlError",
+          data: {
+            error: `wx interpolation unexpected end`,
           },
         },
       ],
