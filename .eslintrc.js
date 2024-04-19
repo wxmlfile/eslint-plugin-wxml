d7cdb515c51fb3f9194f7757c257d88d99e41711
iChenLei <chenleicoder@gmail.com>
2024-04-19 15:28:26 +0800

    fix: raw eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:eslint-plugin/recommended'],
+  extends: ['eslint:recommended', 'plugin:node/recommended', 'plugin:eslint-plugin/recommended'],
   env: {
    es6: true
   }
